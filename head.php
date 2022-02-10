4f0fe6feca14453221e55a6b2a853500c1e7f07a
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2022-02-09 18:53:04 -0800

    Add files via upload

---
+++ head.php
@@ -0,0 +1,9 @@
+  <head>
+    <meta charset="utf-8">
+    <title>Resume</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,400;0,800;1,400;1,800&family=Saira+Condensed:wght@500;700&family=Saira+Extra+Condensed:wght@500;600;700&display=swap" rel="stylesheet">
+    <link href="https://youli.academic.csusb.edu/css/style.css" rel="stylesheet"></link>
+  </head>
--- index.php
+++ index.php
@@ -1,4 +1,42 @@
-<?php
-   $name = "David Brown";
-   echo "Hello " , $name;
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<?php include 'head.php' ?>
+  <body data-bs-spy="scroll" data-bs-target="#navbar-example3" style="position:relative" >
+    <div class="left-nav">
+      <nav id="navbar-example3" class="navbar vertical-center flex-column center">
+        <nav class="nav flex-column">
+          <a class="nav-link" href="#about">About</a>
+          <a class="nav-link" href="#education">Education</a>
+          <a class="nav-link" href="#experience">Experience</a>
+        </nav>
+      </nav>
+    </div>
+    <div data-bs-offset="0" tabindex="0">
+      <div id="about" class="content-item">
+        <div class="vertical-center">
+          <h1>David Brown</h1>
+          <div class="subheading">
+            5500 University Pkwy, San Bernardino, CA 92407 · (123)456-7890
+          </div>
+          <p>...</p>
+        </div>
+      </div>
+      <hr>
+      <div id="education" class="content-item">
+        <div class="vertical-center">
+          <h4>Education</h4>
+          <p>...</p>
+        </div>
+      </div>
+      <hr>
+      <div id="experience" class="content-item">
+        <div class="vertical-center">
+          <h4>Experience</h4>
+          <p>...</p>
+        </div>
+      </div>
+      <hr>
+    </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  </body>
+</html>
