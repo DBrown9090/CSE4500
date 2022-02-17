12fbb92ba4d833266b8c50fb2c20d01953bc1c13
DBrown9090 <91346038+DBrown9090@users.noreply.github.com>
2022-02-16 16:06:33 -0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-<?php include 'head.php';
+<?php
   
   $json_file = file_get_contents('my_data.json');
   $json_data = json_decode($json_file, true);
   
-  
-  ?>
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Resume</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mulish:ital,wght@0,400;0,800;1,400;1,800&family=Saira+Condensed:wght@500;700&family=Saira+Extra+Condensed:wght@500;600;700&display=swap" rel="stylesheet">
+    <link href="https://youli.academic.csusb.edu/css/style.css" rel="stylesheet"></link>
+  </head>
   <body data-bs-spy="scroll" data-bs-target="#navbar-example3" style="position:relative" >
     <div class="left-nav">
       <nav id="navbar-example3" class="navbar vertical-center flex-column center">
